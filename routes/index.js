97e97e75df9681fe80921f8089af578c4534acb8
Hyun A Kim <hyuna@Violet.local>
2024-01-05 00:33:16 +0900

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  // res.render("index", { title: "Express" });
+  const returnValue = { message: "Hello, this is your return value!" };
+
+  res.json(returnValue);
 });
 
 module.exports = router;
